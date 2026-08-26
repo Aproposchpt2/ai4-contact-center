54300effbc28820628bd7aca9945e917690d343d
Aproposchpt2 <157073040+Aproposchpt2@users.noreply.github.com>
2026-08-26 14:12:31 -0700

    UI: declare responsive viewport for tablet rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Plus_Jakarta_Sans, Space_Grotesk } from 'next/font/google';
 import '../styles/globals.css';
 
@@ -17,6 +18,9 @@ const spaceGrotesk = Space_Grotesk({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${plusJakarta.variable} ${spaceGrotesk.variable}`}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
